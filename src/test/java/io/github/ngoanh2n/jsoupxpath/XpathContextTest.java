c272acf0822abc8bd8ec610f191b5c22502f3179
Ngoan Ho <ngoanh2n@gmail.com>
2021-01-16 14:06:50 +0700

    Add XpathContextTest

--- src/test/java/io/github/ngoanh2n/jsoupxpath/XpathContextTest.java
+++ src/test/java/io/github/ngoanh2n/jsoupxpath/XpathContextTest.java
@@ -1,11 +1,10 @@
 package io.github.ngoanh2n.jsoupxpath;
 
 import org.jsoup.nodes.Element;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
@@ -15,24 +14,38 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class XpathContextTest extends AbstractTest {
 
+    XpathContext context;
+
     @Test
-    void test1() {
-        XpathContext context = new XpathContext(document);
+    @Order(1)
+    void invalidTest() {
+        Element element = context.findElement("html/body/div[2]");
+        assertNull(element);
+    }
+
+    @Order(2)
+    @Test
+    void validTest1() {
         Element element = context.findElement("html/body/div/input");
         assertNotNull(element);
     }
 
+    @Order(3)
     @Test
-    void test2() {
-        XpathContext context = new XpathContext(document);
+    void validTest2() {
         Element element = context.findElement("/html/body/div/input[2]");
         assertNotNull(element);
     }
 
+    @Order(4)
     @Test
-    void test3() {
-        XpathContext context = new XpathContext(document);
+    void validTest3() {
         Element element = context.findElement("//html[1]/body[1]/div[1]/input[3]");
         assertNotNull(element);
+    }
+
+    @BeforeEach
+    void createXpathContext() {
+        context = new XpathContext(document);
     }
 }
