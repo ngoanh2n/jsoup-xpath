c3a2328f442d8b2d37d532af9591d8aae9f0907a
Ngoan Ho <ngoanh2n@gmail.com>
2021-01-16 10:49:44 +0700

    Add XpathEvaluators

---
+++ src/main/java/io/github/ngoanh2n/jsoupxpath/XpathEvaluators.java
@@ -0,0 +1,16 @@
+package io.github.ngoanh2n.jsoupxpath;
+
+import org.jsoup.select.Evaluator;
+
+/**
+ * Provides {@linkplain Evaluator}s to build {@linkplain XpathEvaluator}
+ * <br>
+ * <br>
+ * Repository: <a href="https://github.com/ngoanh2n/jsoup-xpath">https://github.com/ngoanh2n/jsoup-xpath</a>
+ *
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 1.0.0
+ * @since 2021-01-16
+ */
+class XpathEvaluators {
+}
