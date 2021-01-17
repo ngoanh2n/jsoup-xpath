fb0ac900c1a8326d852c1d68a5d31d241681c274
Ngoan Ho <ngoanh2n@gmail.com>
2021-01-17 19:49:29 +0700

    Use io.github.ngoanh2n.commons.Resources

--- src/test/java/io/github/ngoanh2n/jsoupxpath/AbstractTest.java
+++ src/test/java/io/github/ngoanh2n/jsoupxpath/AbstractTest.java
@@ -1,12 +1,12 @@
 package io.github.ngoanh2n.jsoupxpath;
 
+import io.github.ngoanh2n.commons.Resources;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -23,17 +23,10 @@ public abstract class AbstractTest {
 
     @BeforeAll
     static void loadDummyHtml() throws IOException {
-        ClassLoader classLoader = AbstractTest.class.getClassLoader();
-        URL resource = classLoader.getResource("io/github/ngoanh2n/jsoupxpath/dummy.html");
-
-        if (resource == null) {
-            throw new IllegalArgumentException("Dummy HTML not found!");
-        } else {
-            File html = new File(resource.getFile());
-            StringBuilder sb = new StringBuilder();
-            Stream<String> lines = Files.lines(Paths.get(html.getAbsolutePath()), StandardCharsets.UTF_8);
-            lines.forEach(sb::append);
-            document = Jsoup.parse(sb.toString());
-        }
+        File html = Resources.file("io/github/ngoanh2n/jsoupxpath/dummy.html");
+        StringBuilder sb = new StringBuilder();
+        Stream<String> lines = Files.lines(Paths.get(html.getAbsolutePath()), StandardCharsets.UTF_8);
+        lines.forEach(sb::append);
+        document = Jsoup.parse(sb.toString());
     }
 }
